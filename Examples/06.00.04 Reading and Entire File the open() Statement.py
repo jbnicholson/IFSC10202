6e587614bf0c62b78a55e97cef52615a8cdc057f
Justin <jbnicholson@ualr.edu>
2025-10-01 14:20:20 +0000

    commit

--- Examples/06.00.04 Reading and Entire File the open() Statement.py
+++ Examples/06.00.04 Reading and Entire File the open() Statement.py
@@ -1,3 +1,4 @@
+
 # Open a file for reading
 demotextfile = open("06.00.00 DemoText.txt", "r")
 # Read the entire contents of the file into a single string
---
+++ Practice.py
@@ -0,0 +1,26 @@
+input_file = "06.Project Input File.txt"
+merge_file = "06.Project Merge File.txt"
+output_file = "06.Project Output File.txt"
+
+# counters
+input_count = 0
+merge_count = 0
+output_count = 0
+
+with open(input_file, "r") as fin, open(output_file, "w") as fout:
+    for line in fin:
+        input_count += 1
+        if "**Insert Merge File Here**" in line:
+            # instead of writing this line, merge in the other file
+            with open(merge_file, "r") as fmerge:
+                for mline in fmerge:
+                    fout.write(mline)
+                    merge_count += 1
+                    output_count += 1
+        else:
+            fout.write(line)
+            output_count += 1
+
+print(f"{input_count} input file records")
+print(f"{merge_count} merge file records")
+print(f"{output_count} output file records")
