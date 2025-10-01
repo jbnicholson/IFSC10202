566445212bfadef8d8e3858fa75c9d6c8f991f6b
Justin <jbnicholson@ualr.edu>
2025-10-01 14:45:52 +0000

    commit

--- Project/06.Project/06.Project.py
+++ Project/06.Project/06.Project.py
@@ -7,18 +7,26 @@ input_count = 0
 merge_count = 0
 output_count = 0
 
-with open(input_file, "r") as fin, open(output_file, "w") as fout:
-    for line in fin:
-        input_count += 1
-        if "**Insert Merge File Here**" in line:
-            with open(merge_file, "r") as fmerge:
-                for mline in fmerge:
-                    fout.write(mline)
-                    merge_count += 1
-                    output_count += 1
-        else:
-            fout.write(line)
-            output_count += 1
+with open(output_file, "w") as output_file:
+    with open(input_file, "r") as input_file:
+        for line in input_file:
+            input_count += 1
+        #check for a merge trigger
+            if "**Insert Merge File Here**" in line:
+                output_file.write(line)
+                output_count += 1
+                #open the merge file and copy the contents
+                with open(merge_file, "r") as merge_file:
+                    for merge_line in merge_file:
+                        output_file.write(merge_line)
+                        merge_count += 1
+                        output_count += 1
+            else:
+                output_file.write(line)
+                output_count += 1
+
+input_file.close()
+output_file.close()
 
 print(f"{input_count} input file records")
 print(f"{merge_count} merge file records")
