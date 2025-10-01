187a28d09a02d029264e11b82e0795edaffcb352
Justin <jbnicholson@ualr.edu>
2025-10-01 05:04:37 +0000

    commit

--- Examples/06.00.04 Reading and Entire File the open() Statement.py
+++ Examples/06.00.04 Reading and Entire File the open() Statement.py
@@ -1,1 +1,8 @@
-demofile = open('06.00.00 DemoText.txt')
+# Open a file for reading
+demotextfile = open("06.00.00 DemoText.txt", "r")
+# Read the entire contents of the file into a single string
+x = demotextfile.read()
+# Print the contents - Note the embedded linefeeds
+print(x)
+# Close the file
+demotextfile.close()
